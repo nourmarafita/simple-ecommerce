af02fc54c5fc0d3566f6ae80135776aa3d89ad8c
Nourma Rafita Sari <nmarafita@gmail.com>
2024-01-24 16:37:03 +0700

    adding navigation

---
+++ src/Products/Products.js
@@ -0,0 +1,8 @@
+import './Products.css';
+function Products() {
+  return (
+    <div>products</div>
+  )
+}
+
+export default Products;
---
+++ src/db/data.js
@@ -0,0 +1,49 @@
+const data = [
+    {
+      id: 1,
+      name: 'Asus Zenfone 9 8/256GB - Black',
+      category: 'Smartphone',
+      description: 'Powerful and lightweight laptop with high-resolution display.',
+      price: 1299.99,
+      quantity: 10,
+      imageUrl: 'https://cdn.eraspace.com/media/catalog/product/z/e/zenfone_9_black1_1.jpg',
+    },
+    {
+      id: 2,
+      name: 'Smartphone Samsung Galaxy S21',
+      category: 'Smartphone',
+      description: 'Latest flagship smartphone with advanced camera features.',
+      price: 899.99,
+      quantity: 15,
+      imageUrl: 'https://cdn.eraspace.com/media/catalog/product/s/a/samsung_galaxy_s21_phantom_black_1_1_1_1.jpg',
+    },
+    {
+      id: 3,
+      name: 'Wireless Bluetooth Headphones',
+      category: 'Accessories',
+      description: 'Over-ear headphones with noise-canceling technology.',
+      price: 149.99,
+      quantity: 20,
+      imageUrl: 'https://cdn.eraspace.com/media/catalog/product/m/o/morejoy_jouirbuds_m_tws_black_1_1_1.jpg',
+    },
+    {
+      id: 4,
+      name: 'Men\'s Running Shoes',
+      category: 'Fashion',
+      description: 'Comfortable and durable shoes for running and workouts.',
+      price: 79.99,
+      quantity: 30,
+      imageUrl: 'https://i1.adis.ws/i/jpl/jd_3MD30321393_a?w=700&resmode=sharp&qlt=70&fmt=webp',
+    },
+    {
+      id: 5,
+      name: 'Fashionable Handbag',
+      category: 'Fashion',
+      description: 'Stylish and spacious handbag for various occasions.',
+      price: 19.99,
+      quantity: 25,
+      imageUrl: 'https://cdn.eraspace.com/media/catalog/product/i/c/ic42604035702581.jpg',
+    },
+  ];
+
+export default data;
